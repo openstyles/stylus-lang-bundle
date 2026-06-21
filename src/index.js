64b36de5471ce6aa691f7c7480cdd297c4add01a
tophf <tophff@gmail.com>
2026-06-21 20:29:53 +0300

    0.64.0 UMD bundle v2 with Compiler, Visitor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+const Compiler = require("stylus/lib/visitor/compiler");
 const Evaluator = require("stylus/lib/visitor/evaluator");
 const Parser = require("stylus/lib/parser");
 const {Renderer} = require("stylus/lib/renderer");
 const nodes = require("stylus/lib/nodes");
+const Visitor = require("stylus/lib/visitor");
 
 const stylus = (str, options) => new Renderer(str, options);
+stylus.Compiler = Compiler;
 stylus.Evaluator = Evaluator;
 stylus.Parser = Parser;
 stylus.Renderer = Renderer;
+stylus.Visitor = Visitor;
 stylus.nodes = nodes;
 
 module.exports = stylus;
