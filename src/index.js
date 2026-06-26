ad941359ffd800eb6a16fa8459902573e7d5e240
tophf <tophff@gmail.com>
2026-06-27 02:21:03 +0300

    keep hoisted media within @-moz-document

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const Compiler = require("stylus/lib/visitor/compiler");
 const Evaluator = require("stylus/lib/visitor/evaluator");
+const Normalizer = require("stylus/lib/visitor/normalizer");
 const Parser = require("stylus/lib/parser");
 const {Renderer} = require("stylus/lib/renderer");
 const nodes = require("stylus/lib/nodes");
@@ -8,6 +9,7 @@ const Visitor = require("stylus/lib/visitor");
 const stylus = (str, options) => new Renderer(str, options);
 stylus.Compiler = Compiler;
 stylus.Evaluator = Evaluator;
+stylus.Normalizer = Normalizer;
 stylus.Parser = Parser;
 stylus.Renderer = Renderer;
 stylus.Visitor = Visitor;
